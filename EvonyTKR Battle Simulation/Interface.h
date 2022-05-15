337d6a9830447aa44122a5aebebad3472ff230f8
Kat-tulpa <56337080+Kat-tulpa@users.noreply.github.com>
2022-05-14 18:18:53 -0700

    Added functionality for setting attacker/defender scalar and flat buffs at start of simulation

--- EvonyTKR Battle Simulation/Interface.h
+++ EvonyTKR Battle Simulation/Interface.h
@@ -1,7 +1,38 @@
 #pragma once
 
-class Interface {
-public:
+#include "Players.h"
 
+class Interface {
 private:
+
+public:
+	static void attackerDefaults() {
+		// BUFF FORMAT: ATK, DEF, HP
+		// Init Attacker % Buffs
+		attacker.buffs().scalar().setMounted(0.0f, 0.0f, 0.0f);
+		attacker.buffs().scalar().setGround(0.0f, 0.0f, 0.0f);
+		attacker.buffs().scalar().setRanged(0.0f, 0.0f, 0.0f);
+		attacker.buffs().scalar().setSiege(0.0f, 0.0f, 0.0f);
+
+		// Init Attacker % Buffs
+		attacker.buffs().flat().setMounted(0, 0, 0);
+		attacker.buffs().flat().setGround(0, 0, 0);
+		attacker.buffs().flat().setRanged(0, 0, 0);
+		attacker.buffs().flat().setSiege(0, 0, 0);
+	}
+
+	static void defenderDefaults() {
+		// BUFF FORMAT: ATK, DEF, HP
+		// Init Defender % Buffs
+		defender.buffs().scalar().setMounted(0.0f, 0.0f, 0.0f);
+		defender.buffs().scalar().setGround(0.0f, 0.0f, 0.0f);
+		defender.buffs().scalar().setRanged(0.0f, 0.0f, 0.0f);
+		defender.buffs().scalar().setSiege(0.0f, 0.0f, 0.0f);
+
+		// Init Defender % Buffs
+		defender.buffs().flat().setMounted(0, 0, 0);
+		defender.buffs().flat().setGround(0, 0, 0);
+		defender.buffs().flat().setRanged(0, 0, 0);
+		defender.buffs().flat().setSiege(0, 0, 0);
+	}
 } interface;
